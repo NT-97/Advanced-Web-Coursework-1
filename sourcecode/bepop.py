217362d1ff9e8623fea8356ed8a83f53c8f4ccae
40203073 <40203073@live.napier.ac.uk>
2018-10-17 17:58:26 +0100

    all information on all templates complete, now to move onto the report and check all features are working correctly

--- sourcecode/bepop.py
+++ sourcecode/bepop.py
@@ -86,5 +86,10 @@ def thebeatles():
 def oasis():
   return render_template('oasis.html')
 
+#Handles the error if the user goes to a domain that does not exist
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template('404.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
