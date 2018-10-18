0836ce2f182d8e70506c408c44c9ee8f7588a86b
40203073 <40203073@live.napier.ac.uk>
2018-10-18 15:50:17 +0100

    all music links added to each album and re designed the main page

--- sourcecode/bepop.py
+++ sourcecode/bepop.py
@@ -17,36 +17,36 @@ def grid():
 def nirvana():
     return render_template('nirvana.html'), 200
 
-@app.route("/music/kaiserchiefs")
+@app.route("/music/kaiser_chiefs")
 def kaiser():
    return render_template('kaiser.html'), 200
 
-@app.route("/music/thekillers")
+@app.route("/music/the_killers")
 def killers():
   return render_template('killers.html'), 200
 
-@app.route("/music/elvispressley")
+@app.route("/music/elvis_pressley")
 def elvis():
   return render_template('elvis.html'), 200
 
-@app.route("/music/blackeyedpeas")
+@app.route("/music/black_eyed_peas")
 def blackeyedpeas():
     return render_template('blackeyedpeas.html')
 
 
-@app.route("/music/theclash")
+@app.route("/music/the_clash")
 def theclash():
     return render_template('theclash.html')
 
-@app.route("/music/takethat")
+@app.route("/music/take_that")
 def takethat():
     return render_template('takethat.html')
 
-@app.route("/music/thestoneroses")
+@app.route("/music/the_stone_roses")
 def thestoneroses():
     return render_template('thestoneroses.html')
 
-@app.route("/music/michaeljackson")
+@app.route("/music/michael_jackson")
 def michaeljackson():
     return render_template('michaeljackson.html')
 
@@ -70,7 +70,7 @@ def razorlight():
 def westlife():
   return render_template('westlife.html')
 
-@app.route("/music/greenday")
+@app.route("/music/green_day")
 def greenday():
   return render_template('greenday.html')
 
@@ -78,7 +78,7 @@ def greenday():
 def radiohead():
   return render_template('radiohead.html')
 
-@app.route("/music/thebeatles")
+@app.route("/music/the_beatles")
 def thebeatles():
   return render_template('thebeatles.html')
 
@@ -87,9 +87,9 @@ def oasis():
   return render_template('oasis.html')
 
 #Handles the error if the user goes to a domain that does not exist
-@app.errorhandler(404)
-def page_not_found(error):
-  return render_template('404.html')
+#@app.errorhandler(404)
+#def page_not_found(error):
+ # return render_template('404.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
