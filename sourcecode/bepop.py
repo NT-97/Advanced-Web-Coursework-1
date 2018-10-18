f4667b0d51fecb8fb3e09d46052f9782390af116
40203073 <40203073@live.napier.ac.uk>
2018-10-18 18:38:41 +0100

    working on the 404 error page

--- sourcecode/bepop.py
+++ sourcecode/bepop.py
@@ -87,9 +87,9 @@ def oasis():
   return render_template('oasis.html')
 
 #Handles the error if the user goes to a domain that does not exist
-#@app.errorhandler(404)
-#def page_not_found(error):
- # return render_template('404.html')
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
